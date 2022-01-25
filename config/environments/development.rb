35a050369dcc9706dfe951afeb9fff8bf762536f
Eric Reyes <ericreyes@gmail.com>
2022-01-25 14:06:55 -0600

    clean up yml files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   #devise said to add:
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
