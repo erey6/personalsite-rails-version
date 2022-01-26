3410452f5bb769a928ac6d4744fd5dfc4a40b04e
Eric Reyes <ericreyes@gmail.com>
2022-01-25 22:09:14 -0600

    added redirects for resume

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   #get 'home/index'
   root "home#index"
   get "home/about"
+  #external redirects for portfiolio apps
+  get "/coffee" => redirect("https://ericapp1.herokuapp.com/coffees/home")
+  get "/calorie" => redirect("https://stoic-fermat-358370.netlify.app/")
+  get "/calories" => redirect("https://stoic-fermat-358370.netlify.app/")
+  get "/trees" => redirect("https://trees-app-a086f6.netlify.app/")
+  get "/flashcards" => redirect("https://flashcardreact.azurewebsites.net")
+  get "/coffee" => redirect("https://ericapp1.herokuapp.com/coffees/home")
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
